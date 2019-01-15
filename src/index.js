0163d6729b54eca8cae175eb243e7eba135a16af
Dersuha <dersuha@mail.ru>
2019-01-15 19:36:30 +0800

    Add index.js

---
+++ src/index.js
@@ -0,0 +1,8 @@
+import readlineSync from 'readline-sync';
+
+console.log('Welcome to the Brain Games! \n');
+const greeting = () => {
+    const userName = readlineSync.question('May I have your name? ');
+    console.log(`Hello, ${userName}!`);
+};
+export default greeting;
