508fef498eb7bcc42cd8f1512f3fc3033e799b48
Dersuha <dersuha@mail.ru>
2019-01-17 09:34:46 +0800

    Fix mentor comments

--- src/even.js
+++ src/even.js
@@ -1,24 +1,24 @@
 import readlineSync from 'readline-sync';
 
+const getNumber = (min, max) => Math.floor(Math.random() * (max - min + 1)) + min;
+const isEven = value => value % 2 === 0;
+const numberOfRounds = 3;
+const minValue = 1;
+const maxValue = 100;
+const greeting = 'Welcome to the Brain Games!';
+const description = 'Answer "yes" if number even otherwise answer "no".\n';
+
 const startGame = () => {
-  console.log('Welcome to the Brain Games! \nAnswer "yes" if number even otherwise answer "no". \n');
+  console.log(`${greeting}`);
+  console.log(`${description}`);
   const userName = readlineSync.question('May I have your name? ');
   console.log(`Hello, ${userName}!\n`);
 
-  const number = () => {
-    const getNumber = (min, max) => Math.floor(Math.random() * (max - min + 1)) + min;
-    return getNumber(1, 666);
-  };
-
-  const isEven = value => value % 2 === 0;
-
-  const numberOfRounds = 3;
-
   for (let i = 0; i < numberOfRounds; i += 1) {
-    const num = number();
-    console.log(`Question: ${num}`);
+    const question = getNumber(minValue, maxValue);
+    console.log(`Question: ${question}`);
     const answer = readlineSync.question('Your answer: ');
-    const rightAnswer = isEven(num) ? 'yes' : 'no';
+    const rightAnswer = isEven(question) ? 'yes' : 'no';
     if (answer === rightAnswer) {
       console.log('Correct!');
     } else {
